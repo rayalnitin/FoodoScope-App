067f34dbe683ae3669424485eaf490860c32751c
saurav kumar <msauravkumar9@gmail.com>
2025-03-24 15:57:57 +0530

    made some changes in index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        backgroundColor:"grey",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>FoodScope App Loading...</Text>
+    </View>
+  );
+}
